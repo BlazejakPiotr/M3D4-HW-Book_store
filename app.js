1e561e9c879c85182ec470128c0405eda3445270
BlazejakPiotr <piotr.blazejak@yahoo.com>
2021-08-12 14:28:21 +0200

    displaying books on website

--- app.js
+++ app.js
@@ -1,10 +1,38 @@
+let booksArr = [];
+
+const row = document.querySelector(".row");
+
 window.onload = () => {
   fetch("https://striveschool-api.herokuapp.com/books")
     .then((response) => response.json())
-    .then((books) => bookData(books))
+    .then((books) => {
+      booksArr = books;
+      displayBooks();
+    })
     .catch((err) => console.log(err));
 };
 
-function bookData(books) {
-  console.log(books[0]);
+function displayBooks() {
+  booksArr.forEach((book) => {
+    const bookNode = document.createElement("div");
+    bookNode.classList.add(
+      "col-12",
+      "col-sm-6",
+      "col-md-4",
+      "col-lg-3",
+      "mb-4"
+    );
+    bookNode.innerHTML = `<div class="card h-100">
+            <img src="${book.img}" class="card-img-top" alt="${book.title}" />
+            <div class="card-body">
+              <h5 class="card-title">${book.title}</h5>
+              <p class="card-text">
+                Category: ${book.category}<br />
+                Price: ${book.price}
+              </p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>`;
+    row.appendChild(bookNode);
+  });
 }
